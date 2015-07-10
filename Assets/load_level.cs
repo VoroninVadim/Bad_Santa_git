c6f04d2882d4acdda610623214e0702ef8556ca9
VoroninVadim <voronin.vadim.i@gmail.com>
2015-07-10 21:55:06 +0600

    Load level

---
+++ Assets/load_level.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System;
+using System.Xml;
+using System.Xml.Linq;
+using System.Collections;
+
+public class load_level : MonoBehaviour {
+
+
+
+    public GameObject bomo;
+	// Use this for initialization
+	void Start () {
+        LevelLoad();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+	}
+
+    void  LevelLoad ()
+    {
+       // XmlTextReader  XmlReader= new XmlTextReader(@"Assets\Scene\level\level.xml");
+
+        try
+        {
+
+    
+        XDocument lvl =  XDocument.Load(@"Assets\Scene\level\level.xml");
+        foreach (XElement level_1_el in lvl.Root.Elements())
+        {
+            switch (level_1_el.Name.ToString())
+            {
+                case "boom":
+                    print("boom");
+                    break;
+                default:
+                    break;
+
+              }
+          
+           }
+
+
+          }
+        catch (XmlException)
+        {
+            print("Wrong   loading level");
+        }
+
+    }
+}
---
+++ Assets/santa_controller.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class santa_controller : MonoBehaviour {
+
+	// Use this for initialization
+    private bool active=true;
+    private int Dx;
+    private int Dy;
+    private Animator Santa_anim;
+    private Rigidbody2D San_Speed;
+
+    public float speed_santa = 1f;   
+
+	void Start () {
+        Santa_anim = GetComponent<Animator>();
+        San_Speed = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (active)
+        {
+            Dx = (int)Input.GetAxisRaw("Horizontal");
+            Dy = (int)Input.GetAxisRaw("Vertical");
+
+
+            if (0 != Dx)  // чтобы не каталось по диагональке.
+                Dy = 0;
+
+            SetAnimation(Dx, Dy);
+           MoveSanta (Dx, Dy);
+
+            if (Input.GetButtonDown("Jump"))
+            {
+               // if (can_shoot == true)
+                 //   StartCoroutine(ShootCoroutine()); ;
+            }
+        }
+
+
+
+	}
+
+
+    void SetAnimation(int x, int y)
+    {
+        Santa_anim.SetFloat("Diraction", (Mathf.Abs(x) - Mathf.Abs(y)));  //If >0  move_forward , if less? then  move  vert
+    }
+
+      void MoveSanta (int x,  int y )
+    {
+        San_Speed.velocity = new Vector2(speed_santa * x, speed_santa * y);
+      }
+
+
+  //  private IEnumerator ShootCoroutine()
+   // {
+
+      //  can_shoot = false;
+     //   Bulet_start();
+      //  yield return new WaitForSeconds(fire_beetween_time);
+
+      //  can_shoot = true;
+   // }
+
+
+}
